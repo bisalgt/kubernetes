63fd692e24a23e5320a70bbff09dbb95519dc386
bisalgt <bisalgt@gmail.com>
2022-09-29 22:40:21 +0200

    Django can communicate with mysql using service because django and mysql run in same node -- minikube--. Need to configure mysql so that database are in sync and only one db can read/write and rest db can read only

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'mysql',
+        'PORT': '3306',
     }
 }
 
